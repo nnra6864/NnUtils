f12fb80b34c629d17eefe28f3b18738fdc46c022
nnra6864 <enenragd@gmail.com>
2024-12-14 18:36:21 +0100

    Added EnvelopedImage

---
+++ Scripts/UI/EnvelopedImage.cs
@@ -0,0 +1,47 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NnUtils.Scripts.UI
+{
+    [ExecuteAlways]
+    [RequireComponent(typeof(AspectRatioFitter))]
+    public class EnvelopedImage : Image
+    {
+        private Sprite _currentSprite;
+        [HideInInspector] [SerializeField] private AspectRatioFitter _aspectRatioFitter;
+
+        protected override void Reset()
+        {
+            _aspectRatioFitter            = GetComponent<AspectRatioFitter>();
+            _aspectRatioFitter.aspectMode = AspectRatioFitter.AspectMode.EnvelopeParent;
+            base.Reset();
+        }
+        
+        private void Update()
+        {
+            if (sprite == _currentSprite) return;
+            _currentSprite = sprite;
+            if (_currentSprite == null) return;
+            var rect = _currentSprite.rect;
+            _aspectRatioFitter.aspectRatio = rect.width / rect.height;
+        }
+
+        [MenuItem(itemName: "GameObject/UI/EnvelopedImage")]
+        private static void CreateEnvelopedImage()
+        {
+            // Add the EnvelopedImage to the scene
+            var obj = new GameObject("EnvelopedImage");
+            obj.AddComponent<EnvelopedImage>();
+            
+            // Set its parent to the active object if not null
+            obj.transform.SetParent(Selection.activeTransform, false);
+
+            // Select the newly created object
+            Selection.activeTransform = obj.transform;
+            
+            // Set object as dirty to ensure it's saved
+            EditorUtility.SetDirty(obj);
+        }
+    }
+}
