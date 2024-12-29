4095652e40169bacd09be17d42818a4d27656bcb
nnra6864 <enenragd@gmail.com>
2024-12-29 15:02:02 +0100

    Reimplemented FileSystemMonitor as a wrapper, not a multi-purpose class, much better

--- Scripts/FileSystemMonitor.cs
+++ Scripts/FileSystemMonitor.cs
@@ -1,15 +1,11 @@
 using System;
 using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-using UnityEngine;
 
 namespace NnUtils.Scripts
 {
-    /// Wrapper for the <see cref="FileSystemWatcher"/> with qol changes
+    /// Wrapper for the <see cref="FileSystemWatcher"/> with qol changes <br/>
     public class FileSystemMonitor : IDisposable
     {
-        private const float DefaultReloadDelay = 0.1f;
         private const NotifyFilters DefaultFilters = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.Size;
         
         /// Used to detect file changes
@@ -23,18 +19,12 @@ namespace NnUtils.Scripts
 
         /// <see cref="Directory"/> and <see cref="File"/> combined
         public readonly string Path;
-
-        /// Used to avoid multiple detections at once
-        public readonly int ReloadDelay;
         
         /// Notify Filters
         public readonly NotifyFilters NotifyFilter;
 
         /// Gets executed when fs changes
         public event Action OnChanged;
-
-        /// Used to execute OnChanged on the main thread to avoid weird behaviour
-        private readonly SynchronizationContext _sync;
 
         /// <summary>
         /// Creates a new <see cref="FileSystemMonitor"/>
@@ -43,8 +33,7 @@ namespace NnUtils.Scripts
         /// <param name="file">Full path to the file you want to monitor</param>
         /// <param name="onChanged">Gets executed when the change is detected</param>
         /// <param name="notifyFilter">Notify filters</param>
-        /// <param name="reloadDelay">Delay before the change is acknowledged</param>
-        public FileSystemMonitor(string directory, string file = "*.*", Action onChanged = null, float reloadDelay = DefaultReloadDelay, NotifyFilters notifyFilter = DefaultFilters)
+        public FileSystemMonitor(string directory, string file = "*.*", Action onChanged = null, NotifyFilters notifyFilter = DefaultFilters)
         {
             // Store values
             Directory    = directory;
@@ -52,10 +41,6 @@ namespace NnUtils.Scripts
             Path         = System.IO.Path.Combine(Directory, File);
             OnChanged    = onChanged;
             NotifyFilter = notifyFilter;
-            ReloadDelay  = (int)(reloadDelay * 1000);
-
-            // Get sync context
-            _sync = SynchronizationContext.Current;
             
             // Initialize watcher
             InitializeWatcher();
@@ -70,9 +55,8 @@ namespace NnUtils.Scripts
         /// <param name="path">Full path to the directory or file you want to monitor</param>
         /// <param name="onChanged">Gets executed when the change is detected</param>
         /// <param name="notifyFilter">Notify filters</param>
-        /// <param name="reloadDelay">Delay before the change is acknowledged</param>
-        public FileSystemMonitor(string path, Action onChanged = null, float reloadDelay = DefaultReloadDelay, NotifyFilters notifyFilter = DefaultFilters)
-        : this(System.IO.Path.GetDirectoryName(path), System.IO.Path.GetFileName(path), onChanged, reloadDelay, notifyFilter) { }
+        public FileSystemMonitor(string path, Action onChanged = null, NotifyFilters notifyFilter = DefaultFilters)
+        : this(System.IO.Path.GetDirectoryName(path), System.IO.Path.GetFileName(path), onChanged, notifyFilter) { }
         
         private void InitializeWatcher() => 
             Watcher = new()
@@ -92,30 +76,10 @@ namespace NnUtils.Scripts
             Watcher.EnableRaisingEvents =  true;
         }
 
-        /// Used to cancel the invocation of <see cref="OnChanged"/> in case the config reloaded again
-        private CancellationTokenSource _cancellationTokenSource;
-        
-        /// Executes <see cref="HandleChange()"/> on the main thread to avoid weird behaviour
-        private void HandleChange(object sender, FileSystemEventArgs args) => _sync.Post(_ => HandleChange(), null);
-
-        /// Handles the change being changed
-        private void HandleChange()
+        /// Invokes the <see cref="OnChanged"/> event
+        private void HandleChange(object sender, FileSystemEventArgs args)
         {
-            // Cancel the previous task if it hasn't completed yet
-            _cancellationTokenSource?.Cancel();
-
-            // Assign a new cancellation token
-            _cancellationTokenSource = new();
-
-            // Trigger the delayed event
-            ChangeTask(_cancellationTokenSource.Token).ConfigureAwait(false);
-        }
-        
-        /// Waits for <see cref="ReloadDelay"/> seconds and triggers the <see cref="OnChanged"/> event
-        private async Task ChangeTask(CancellationToken cancellationToken)
-        {
-            await Task.Delay(ReloadDelay, cancellationToken);
-            if (!cancellationToken.IsCancellationRequested) OnChanged?.Invoke();
+            OnChanged?.Invoke();
         }
 
         #region IDisposable
@@ -129,7 +93,6 @@ namespace NnUtils.Scripts
             
             // Dispose components
             Watcher?.Dispose();
-            _cancellationTokenSource?.Dispose();
             
             // Set _disposed to true
             _disposed = true;
