345668f5f3c43a5a53df42d6a80e67081d9f1b46
nnra6864 <enenragd@gmail.com>
2025-02-10 00:02:29 +0100

    Updated the NnManager

--- Scripts/NnManager.cs
+++ Scripts/NnManager.cs
@@ -1,7 +1,10 @@
+using NnUtils.Modules.JSONUtils.Scripts.Types.Components.UI.Image;
 using UnityEngine;
 
 namespace NnUtils.Scripts
 {
+    [RequireComponent(typeof(TimeManager))]
+    [RequireComponent(typeof(ProjectImageManagerScript))]
     public class NnManager : MonoBehaviour
     {
         private static NnManager _instance;
@@ -9,15 +12,14 @@ namespace NnUtils.Scripts
         {
             get
             {
+                // Return the instance if it's not null
                 if (_instance != null) return _instance;
                 
+                // Try to find an instance and return it
                 _instance = FindFirstObjectByType<NnManager>();
-                if (_instance != null)
-                {
-                    DontDestroyOnLoad(_instance);
-                    return _instance;
-                }
+                if (_instance != null) return _instance;
 
+                // Create a new object, don't destroy it on load, assign it to _instance and return it
                 var go = new GameObject("NnManager");
                 DontDestroyOnLoad(go);
                 _instance = go.AddComponent<NnManager>();
@@ -27,19 +29,26 @@ namespace NnUtils.Scripts
         
         private void Awake()
         {
+            // If no instance is found, assign this and don't destroy it on load
             if (_instance == null)
             {
                 _instance = this;
                 DontDestroyOnLoad(gameObject);
             }
-            else if (_instance != this)
-            {
-                Destroy(gameObject);
-            }
+            // If instance was found, check if it's not this and destroy this game object
+            else if (_instance != this) Destroy(gameObject);
+        }
+
+        private void Reset()
+        {
+            _timeManager = GetComponent<TimeManager>();
+            _imageManager = GetComponent<ProjectImageManagerScript>();
         }
 
         [SerializeField] private TimeManager _timeManager;
-        public static TimeManager TimeManager =>
-            Instance._timeManager ?? (Instance._timeManager = Instance.gameObject.GetOrAddComponent<TimeManager>());
+        public static TimeManager TimeManager => Instance._timeManager;
+
+        [SerializeField] private ProjectImageManagerScript _imageManager;
+        public static ProjectImageManagerScript ImageManager => Instance._imageManager;
     }
 }
