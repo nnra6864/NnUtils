6bfa3cb3676a5e713e90037a15dc8e67b66e2e18
nnra6864 <enenragd@gmail.com>
2025-04-28 10:00:03 +0200

    Adjusted the bind again

--- Scripts/Editor/EditorShortcuts.cs
+++ Scripts/Editor/EditorShortcuts.cs
@@ -6,7 +6,7 @@ namespace NnUtils.Scripts.Editor
 {
     public class EditorShortcuts : MonoBehaviour
     {
-        [Shortcut("NnUtils/New Script", KeyCode.S, ShortcutModifiers.Control)]
+        [Shortcut("NnUtils/New Script", KeyCode.S, ShortcutModifiers.Alt)]
         private static void NewScript() =>
             ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
                 "Assets/NnUtils/Scripts/Editor/NnScriptTemplate.txt", "Script.cs");
